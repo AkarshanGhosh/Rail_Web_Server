df25b521bad883e0ced6a6512804d3f2795a8631
AkarshanGhosh <90913998+AkarshanGhosh@users.noreply.github.com>
2025-07-10 18:48:54 +0530

    Update activityRoutes.js

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,7 +1,7 @@
 // routes/activityRoutes.js
 const express = require('express');
 const router = express.Router();
-const activityController = require('../controllers/ActivityController'); // Adjust path
+const activityController = require('../controller/ActivityController'); // Adjust path
 const userAuth = require('../middleware/UserAuth'); // Your authentication middleware
 
 // Route to get recent activities (Admin only)
